c9be6ad984f3543337c05aa71bec9188b45da4f6
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-21 15:00:32 +0100

    Add Match.jsx component

---
+++ src/screens/Match.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Matches from '../components/card/Matches'
+
+const MatchDetail = () => {
+  return (
+    <View>
+        <Matches />
+    </View>
+  )
+}
+
+export default MatchDetail
