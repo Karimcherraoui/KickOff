c47928bc7e2d9f0991c6709dd4d0759a082bbc35
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-22 23:18:50 +0100

    Add matchId prop to MatchDetailCard component

--- src/screens/MatchesDetailScreen.jsx
+++ src/screens/MatchesDetailScreen.jsx
@@ -3,10 +3,11 @@ import React from "react";
 import MatchDetailCard from "../components/card/MatchDetailCard";
 
 
-const MatchesDetailScreen = () => {
+const MatchesDetailScreen = (props) => {
+  console.log("matchId",  JSON.stringify( props.route.params.matchId));
   return (
       <View>
-        <MatchDetailCard />
+        <MatchDetailCard matchID = {props.route.params.matchId}/>
       </View>
   );
 };
