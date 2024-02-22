cb49bfc34714723966103dd9c25d67643c438881
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-22 15:45:57 +0100

    Add MatchesDetailScreen component

---
+++ src/screens/MatchesDetailScreen.jsx
@@ -0,0 +1,15 @@
+import { View } from "react-native";
+import React from "react";
+import MatchDetailCard from "../components/card/MatchDetailCard";
+
+
+const MatchesDetailScreen = () => {
+  return (
+      <View>
+        <MatchDetailCard />
+      </View>
+  );
+};
+
+export default MatchesDetailScreen;
+
