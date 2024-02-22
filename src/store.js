1e14a8e9fe04b0c93593cdf9e6b6c940c951704f
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-22 15:44:05 +0100

    Add Leagues reducer to store configuration

--- src/store.js
+++ src/store.js
@@ -1,15 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import matchesReducer from "./features/matchesSlice";
+import leaguesReducer from "./features/leagueSlice";
 
-
-
+const middleware = [...getDefaultMiddleware({
+  serializableCheck: false, 
+})];
 
 const store = configureStore({
   reducer: {
-    matches : matchesReducer,
+    Matches: matchesReducer,
+    Leagues: leaguesReducer,
   },
-  
-});
+  middleware,
 
+});
 
 export default store;
