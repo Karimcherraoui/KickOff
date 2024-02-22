d4ed7bd018d5cecafac583412ecc6262aa8ce4c8
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-22 15:46:08 +0100

    Add PlayerScreen component

---
+++ src/screens/PlayerScreen.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import PlayersCard from "../components/card/PlayersCard";
+
+const PlayerScreen = () => {
+  return (
+    <View>
+      <PlayersCard />
+    </View>
+  );
+};
+
+export default PlayerScreen;
