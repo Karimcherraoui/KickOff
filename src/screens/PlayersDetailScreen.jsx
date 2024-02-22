60b92c7b6b1c0da4428e536e27a58599a77116a1
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-22 15:46:11 +0100

    Add PlayersDetailScreen component

---
+++ src/screens/PlayersDetailScreen.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import PlayerDetailCard from "../components/card/PlayerDetailCard";
+
+const PlayersDetailScreen = () => {
+  return (
+    <View>
+      <PlayerDetailCard />
+    </View>
+  );
+};
+
+export default PlayersDetailScreen;
