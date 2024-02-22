1a1fb05f55dcc725f0a06b805a3115076135d386
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-22 15:45:18 +0100

    Refactor navigation tabs and screens

--- src/components/navigations/Tab.jsx
+++ src/components/navigations/Tab.jsx
@@ -1,68 +1,75 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Drawer from "./Drawer";
-import Player from "../../screens/Player";
-import Match from "../../screens/Match";
 import { StatusBar } from "react-native";
-import MatchDetail from "../../screens/MatchDetail";
-import Matches from "../card/Matches";
 import { FontAwesome } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
-import Favorie from "../../screens/Favorie";
+import MatchScreen from "../../screens/MatchScreen";
+import PlayerScreen from "../../screens/PlayerScreen";
+import FavorieScreen from "../../screens/FavorieScreen";
+import MatchesDetailScreen from "../../screens/MatchesDetailScreen";
+import PlayersDetailScreen from "../../screens/PlayersDetailScreen";
+
 
 const Tab = () => {
   const Tab = createBottomTabNavigator();
 
   return (
     <>
-      <StatusBar hidden={false} barStyle={"black"}  />
+      <StatusBar hidden={false} barStyle={"black"} />
       <LinearGradient
-        colors={['rgba(0,8,38,1)', 'rgba(37,73,115,1)']}
+        colors={["rgba(0,8,38,1)", "rgba(37,73,115,1)"]}
         start={{ x: 0, y: 0 }}
         end={{ x: 1, y: 0 }}
         style={{ flex: 1 }}
       >
-      <Tab.Navigator    screenOptions={{
-          tabBarActiveTintColor: 'red',
-          tabBarInactiveTintColor: 'white',
-          tabBarStyle: {
-            backgroundColor: 'transparent', 
-          },
-        }}>
-        <Tab.Screen
-          name="Matches"
-          component={Matches}
-          options={{
-            tabBarIcon: () => (
-              <FontAwesome name="soccer-ball-o" size={24} color="white" />
-            ),
+        <Tab.Navigator
+          screenOptions={{
+            tabBarActiveTintColor: "red",
+            tabBarInactiveTintColor: "white",
+            tabBarStyle: {
+              backgroundColor: "transparent",
+            },
           }}
-        />
-        <Tab.Screen
-          name="Player"
-          component={Player}
-          options={{
-            tabBarIcon: () => (
-              <FontAwesome name="user" size={24} color="white" />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Favories"
-          component={Favorie}
-          options={{
-            tabBarIcon: () => (
-              <MaterialIcons name="favorite" size={24} color="white" />
-            ),
-          }}
-        />
-        <Tab.Screen
-        name="MatchDetail"
-        component={MatchDetail}
-        options={{ tabBarButton: () => null }} // Hide the tab bar icon
-      />
-      </Tab.Navigator>
+        >
+          <Tab.Screen
+            name="MatchScreen"
+            component={MatchScreen}
+            options={{
+              tabBarIcon: () => (
+                <FontAwesome name="soccer-ball-o" size={24} color="white" />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="PlayerScreen"
+            component={PlayerScreen}
+            options={{
+              tabBarIcon: () => (
+                <FontAwesome name="user" size={24} color="white" />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="FavorieScreen"
+            component={FavorieScreen}
+            options={{
+              tabBarIcon: () => (
+                <MaterialIcons name="favorite" size={24} color="white" />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="MatchesDetailScreen"
+            component={MatchesDetailScreen}
+            options={{ tabBarButton: () => null }} 
+          />
+          <Tab.Screen
+            name="PlayerDetailScreen"
+            component={PlayersDetailScreen}
+            options={{ tabBarButton: () => null }}
+          />
+        </Tab.Navigator>
       </LinearGradient>
     </>
   );
