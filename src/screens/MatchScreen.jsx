8c9470cfdc6de3d8591945ea5f0280ac86a8cced
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-24 22:33:30 +0100

    Refactor MatchScreen component and add useEffect hook

--- src/screens/MatchScreen.jsx
+++ src/screens/MatchScreen.jsx
@@ -1,22 +1,33 @@
-import { View, Text, StyleSheet, ImageBackground } from 'react-native'
-import React from 'react'
-import Matches from '../components/card/MatcheCard'
+import { StyleSheet, ImageBackground } from "react-native";
+import React, { useEffect } from "react";
+import Matches from "../components/card/MatcheCard";
+import { useDispatch } from "react-redux";
+import { clearMatch } from "../features/matchesSlice";
 
 const MatchScreen = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    return () => {
+      dispatch(clearMatch());
+    };
+  }, []);
   return (
-    <ImageBackground style={styles.background} source={require('../../assets/images.jpeg')}>
-    <Matches />
+    <ImageBackground
+      style={styles.background}
+      source={require("../../assets/images.jpeg")}
+    >
+      <Matches />
     </ImageBackground>
-  )
-}
+  );
+};
 
-export default MatchScreen
+export default MatchScreen;
 const styles = StyleSheet.create({
-
   background: {
     flex: 1,
     backgroundColor: "#fff",
     resizeMode: "cover",
-    justifyContent: "center"
+    justifyContent: "center",
   },
 });
