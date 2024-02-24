b4aadce414ecd91851aa73ad7aba020f329b9d50
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-24 22:33:36 +0100

    Refactor PlayersDetailScreen to pass player ID as a prop

--- src/screens/PlayersDetailScreen.jsx
+++ src/screens/PlayersDetailScreen.jsx
@@ -2,10 +2,11 @@ import { View, Text } from "react-native";
 import React from "react";
 import PlayerDetailCard from "../components/card/PlayerDetailCard";
 
-const PlayersDetailScreen = () => {
+const PlayersDetailScreen = (id) => {
+
   return (
-    <View>
-      <PlayerDetailCard />
+    <View >
+      <PlayerDetailCard playerID={id.route.params.playerId}/>
     </View>
   );
 };
