5408fca829eff358bfc87feb70109b184f30535e
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-24 22:33:11 +0100

    Add fetchOneMatche and fetchOnePlayer async thunks***

--- src/features/matchesSlice.js
+++ src/features/matchesSlice.js
@@ -2,6 +2,8 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
   matches: [],
+  players: [],
+  oneMatche: null,
   status: "idle",
   error: null,
 };
@@ -18,7 +20,44 @@ export const fetchMatches = createAsyncThunk(
       }
       
       const data = await response.json();
-      return data;
+      return data.events;
+    } catch (error) {
+      return thunkAPI.rejectWithValue({ error: error.message });
+    }
+  }
+);
+export const fetchOneMatche = createAsyncThunk(
+  "matches/fetchOneMatche",
+  async (matchID, thunkAPI) => {
+    try {
+      const response = await fetch(
+        `https://api.sofascore.com/api/v1/event/${matchID}`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch matches");
+      }
+      
+      const data = await response.json();
+      console.log(data.event);
+      return data.event;
+    } catch (error) {
+      return thunkAPI.rejectWithValue({ error: error.message });
+    }
+  }
+);
+export const fetchOnePlayer = createAsyncThunk(
+  "players/fetchOnePlayer",
+  async (matchID, thunkAPI) => {
+    try {
+      const response = await fetch(
+        `https://api.sofascore.com/api/v1/event/${matchID}/incidents`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch matches");
+      }
+      
+      const data = await response.json();
+      return data.incidents;
     } catch (error) {
       return thunkAPI.rejectWithValue({ error: error.message });
     }
@@ -28,6 +67,9 @@ const matchesSlice = createSlice({
   name: "matches",
   initialState,
   reducers: {
+    clearMatch: (state) => {
+      state.oneMatche = null;
+    },
   },
   extraReducers: (builder) => {
     builder
@@ -43,9 +85,33 @@ const matchesSlice = createSlice({
       .addCase(fetchMatches.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.payload.error;
+      })
+      .addCase(fetchOnePlayer.pending, (state) => {
+        state.status = "loading";
+        state.error = null;
+      })
+      .addCase(fetchOnePlayer.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.players = action.payload;
+      })
+      .addCase(fetchOnePlayer.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload.error;
+      })
+      .addCase(fetchOneMatche.pending, (state) => {
+        state.status = "loading";
+        state.error = null;
+      })
+      .addCase(fetchOneMatche.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.oneMatche = action.payload;
+      })
+      .addCase(fetchOneMatche.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload.error;
       });
   },
 });
-export const { logMatches } = matchesSlice.actions;
+export const { clearMatch } = matchesSlice.actions;
 
 export default matchesSlice.reducer;
