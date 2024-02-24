3ffdd9646b443725ec069438c57a958e13663343
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-24 22:33:25 +0100

    Add useEffect and useDispatch to MatchesDetailScreen

--- src/screens/MatchesDetailScreen.jsx
+++ src/screens/MatchesDetailScreen.jsx
@@ -1,13 +1,20 @@
 import { View } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import MatchDetailCard from "../components/card/MatchDetailCard";
+import { useDispatch } from "react-redux";
+import { clearMatch } from "../features/matchesSlice";
 
 
 const MatchesDetailScreen = (props) => {
   console.log("matchId",  JSON.stringify( props.route.params.matchId));
+  const matchId = props.route.params.matchId;
+  const dispatch = useDispatch();
+  
+
+
   return (
       <View>
-        <MatchDetailCard matchID = {props.route.params.matchId}/>
+        <MatchDetailCard matchID={props.route.params.matchId}/>
       </View>
   );
 };
