3e92a0f2fe0e41452748ff5a0eb872a50909123d
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-24 22:33:19 +0100

    Add FavorieCard component to FavorieScreen

--- src/screens/FavorieScreen.jsx
+++ src/screens/FavorieScreen.jsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import FavorieCard from '../components/card/FavorieCard'
 
 const FavorieScreen = () => {
   return (
     <View>
-      <Text>Screen</Text>
+      <FavorieCard />
     </View>
   )
 }
