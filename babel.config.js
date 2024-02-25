03faa577349e17b41c42f2ee99c7e4d51da6d937
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-25 14:15:38 +0100

    Update Babel configuration to include additional presets and plugins

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo', '@babel/preset-env', '@babel/preset-react'],
+    plugins: ['@babel/plugin-transform-runtime'],
+    env: {
+      test: {
+        presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+        plugins: ['@babel/plugin-transform-modules-commonjs'],
+      },
+    },
   };
 };
