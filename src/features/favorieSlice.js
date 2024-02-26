7c1109e5770ff3bc841460e45eccb11c6b0994fb
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-26 10:22:19 +0100

    Refactor favoriteSlice.js to toggle favorites

--- src/features/favorieSlice.js
+++ src/features/favorieSlice.js
@@ -3,52 +3,25 @@ import axios from "axios";
 
 const initialState = {
   favories: [],
-  status: "idle",
-  error: null,
 };
 
-export const fetchFavories= createAsyncThunk(
-  "favories/fetchFavories",
-  async (matchID, thunkAPI) => {
-    try {
-      const response = await axios.get(
-        `https://api.sofascore.com/api/v1/event/${matchID}`
-      );
-      if (response.status >= 200 && response.status < 300) {
-        return response.data.event;
-      } else {
-        throw new Error("Failed to fetch matches");
-      }
-    } catch (error) {
-      return thunkAPI.rejectWithValue({ error: error.message });
-    }
-  }
-);
+
 
 const favorieSlice = createSlice({
   name: "favories",
   initialState,
   reducers: {
-    removeFavorie: (state, action) => {
-      state.favories = state.favories.filter((item) => item.id !== action.payload);
-    },
+    toggleFavourite: (state, action) => {
+      const index = state.favories.indexOf(action.payload)
+      if (index !== -1) {
+          state.favories.splice(index, 1)
+      } else {
+          state.favories.push(action.payload)
+      }
   },
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchFavories.pending, (state) => {
-        state.status = "loading";
-        state.error = null;
-      })
-      .addCase(fetchFavories.fulfilled, (state, action) => {
-        state.status = "succeeded";
-        state.favories = action.payload;
-      })
-      .addCase(fetchFavories.rejected, (state, action) => {
-        state.status = "failed";
-        state.error = action.payload ? action.payload.error : "Unknown error";
-      });
   },
+  
 });
 
-export const { removeFavorie } = favorieSlice.actions;
+export const { toggleFavourite } = favorieSlice.actions;
 export default favorieSlice.reducer;
