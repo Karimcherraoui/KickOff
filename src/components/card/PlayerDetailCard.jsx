9a84ed6fa32a571522ada9283bc8ca1686c8b9de
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-26 10:21:59 +0100

    Refactor PlayerDetailCard component styles

--- src/components/card/PlayerDetailCard.jsx
+++ src/components/card/PlayerDetailCard.jsx
@@ -1,14 +1,14 @@
 import { View, Text, Image, StyleSheet, ImageBackground } from "react-native";
 import React from "react";
 import { useSelector } from "react-redux";
+import {styles} from "../../styles/playerDetailCard";
+
 
 const PlayerDetailCard = (id) => {
   const playerID = id.playerID;
-  console.log("playerID", playerID);
   const player = useSelector((state) =>
     state.Players.players.find((player) => player.id === playerID)
   );
-  console.log("player", player);
   return (
     <>
       <ImageBackground    source={require("../../../assets/stade.jpeg")} style={styles.container}>
@@ -59,71 +59,3 @@ const PlayerDetailCard = (id) => {
 
 export default PlayerDetailCard;
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center", 
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-    gap: 40,
-  },
-  imageBack: {
-    display: "flex",
-    alignItems: "center",
-    backgroundColor: "#5b8db0",
-    padding: 30,
-    width: "100%",
-    borderBottomEndRadius: 40,
-    borderBottomStartRadius: 40,
-    shadowColor: "black",
-    shadowOffset: {
-      width: 0,
-      height: 12,
-    },
-    shadowOpacity: 0.5,
-  },
-  image: {
-    width: 100,
-    height: 100,
-    marginVertical: 20,
-  },
-  playerName: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "white",
-  },
-  position: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: "gray",
-    textAlign: "center",
-    marginBottom: 10,
-  },
-
-  infoText: {
-    textAlign: "center",
-    color: "black",
-  },
-
-  detail: {
-    display: "flex",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    alignItems: "center",
-    gap: 20,
-  },
-  info: {
-    width: "40%",
-    marginTop: 20,
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 40,
-    shadowColor: "black",
-    shadowOffset: {
-      width: 4,
-      height: 12,
-    },
-    shadowOpacity: 0.6,
-  },
-});
