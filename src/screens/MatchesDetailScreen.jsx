35c2ddf842b8abae160cce424a9724803b0ddb5c
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-26 10:22:44 +0100

    Remove unnecessary code in MatchesDetailScreen.jsx

--- src/screens/MatchesDetailScreen.jsx
+++ src/screens/MatchesDetailScreen.jsx
@@ -6,11 +6,6 @@ import { clearMatch } from "../features/matchesSlice";
 
 
 const MatchesDetailScreen = (props) => {
-  console.log("matchId",  JSON.stringify( props.route.params.matchId));
-  const matchId = props.route.params.matchId;
-  const dispatch = useDispatch();
-  
-
 
   return (
       <View>
