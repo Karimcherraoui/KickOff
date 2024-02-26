63dc981030f0f36baf429a5969ab960697050e16
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-26 10:23:03 +0100

    Add styles for matchDetailCard component

---
+++ src/styles/matchDetailCard.js
@@ -0,0 +1,177 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      backgroundColor: "gray",
+    },
+    league: {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      backgroundColor: "#e0e0e0",
+      padding: 8,
+      borderBottomWidth: 1,
+      borderBottomColor: "#cdcccc",
+      fontWeight: "bold",
+      fontSize: 15,
+      color: "red",
+      gap: 10,
+    },
+    seasonName: {
+      fontSize: 15,
+      color: "black",
+      marginTop: 10,
+      marginBottom: 10,
+      fontWeight: "bold",
+    },
+    detail: {
+      padding: 8,
+      backgroundColor: "#fffbef",
+      borderBottomWidth: 1,
+      borderBottomColor: "#cdcccc",
+    },
+    match: {
+      flexDirection: "row",
+      justifyContent: "center",
+      gap: 50,
+      margin: 5,
+    },
+    temp: {
+      flexDirection: "row",
+      justifyContent: "center",
+      margin: 5,
+    },
+    nameMatch: {
+      fontWeight: "bold",
+      fontSize: 15,
+      color: "black",
+    },
+    textTemp: {
+      fontSize: 15,
+      color: "#bcbcbc",
+      textAlign: "center",
+      marginTop: 10,
+      marginBottom: 20,
+    },
+    dropdown: {
+      color: "white",
+    },
+    icon: {
+      marginRight: 5,
+    },
+    placeholderStyle: {
+      fontSize: 16,
+      color: "white",
+      marginLeft: 20,
+    },
+    selectedTextStyle: {
+      fontSize: 16,
+    },
+    iconStyle: {
+      width: 20,
+      height: 20,
+    },
+    inputSearchStyle: {
+      height: 40,
+      fontSize: 16,
+    },
+    buttonGradient: {
+      borderRadius: 10,
+      padding: 10,
+      height: 50,
+      borderBottomColor: "gray",
+      borderBottomWidth: 0.5,
+      borderRadius: 10,
+      marginVertical: 10,
+      marginHorizontal: 40,
+      padding: 10,
+    },
+    image: {
+      width: 40,
+      height: 40,
+      borderRadius: 25,
+    },
+    team: {
+      flexDirection: "column",
+      alignItems: "center",
+      gap: 10,
+    },
+    teams: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      gap: 20,
+    },
+    resultat: {
+      color: "green",
+      fontSize: 30,
+      fontWeight: "bold",
+      textAlign: "center",
+    },
+    butteurs: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      marginTop: 20,
+      paddingHorizontal: 20,
+    },
+    butteur: {
+      fontSize: 12,
+      fontWeight: "bold",
+      color: "#7a7a7a",
+      marginBottom: 15,
+    },
+    stade: {
+      width: "100%",
+      height: 300,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    round: {
+      color: "#bcbcbc",
+      fontSize: 15,
+      textAlign: "center",
+      marginVertical: 15,
+    },
+  
+    imageTour: {
+      width: 30,
+      height: 30,
+    },
+    butteurContianer: {
+      display: "flex",
+      flexDirection: "row",
+      gap: 10,
+    },
+    stadiumName: {
+      color: "white",
+      fontSize: 40,
+      fontWeight: "bold",
+      marginBottom: 200,
+      padding: 10,
+      borderRadius: 10,
+      borderWidth: 3,
+      borderColor: "orange",
+    },
+    entraineurs: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      paddingHorizontal: 30,
+      paddingVertical: 30,
+    },
+  
+    coach: {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      gap: 10,
+    },
+    coachName: {
+      fontSize: 15,
+      color: "gray",
+      fontWeight: "bold",
+    },
+    
+  });
+  
