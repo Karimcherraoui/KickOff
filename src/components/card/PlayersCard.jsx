fb8ff3f69aa755558ffd6ff8a5500bba3f270f5f
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-26 10:22:04 +0100

    Add styles import to PlayersCard component

--- src/components/card/PlayersCard.jsx
+++ src/components/card/PlayersCard.jsx
@@ -12,6 +12,8 @@ import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import { useSelector } from "react-redux";
 import { clearMatch } from "../../features/matchesSlice";
+import {styles} from "../../styles/playerCard";
+
 
 const PlayersCard = () => {
   const navigate = useNavigation();
@@ -51,46 +53,3 @@ const handleClick = (id) => {
 
 export default PlayersCard;
 
-const styles = StyleSheet.create({
-  container: {
-    padding: 10,
-    marginBottom: 10,
-    borderRadius: 20,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 7,
-    },
-    shadowOpacity: 0.1,
-  },
-  imageBackground: {
-    borderRadius: 10,
-    overflow: "hidden",
-  },
-  pressable: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 10,
-    padding: 20,
-  },
-
-  namePlayer: {
-    fontWeight: "bold",
-    fontSize: 20,
-    color: "white",
-    backgroundColor: "black",
-    padding: 10,
-    borderWidth: 1,
-    borderColor: "white",
-    borderRadius: 10,
-  },
-
-  image: {
-    width: 70,
-    height: 70,
-    borderRadius: 25,
-    backgroundColor: "white",
-
-  },
-});
